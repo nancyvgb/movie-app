3ff079e9c4cac8611b75353f9cb0e09f07e8d201
nancyvgb <nancyguerrerobuelvas@gmail.com>
2020-05-01 11:08:14 -0500

    created movie app
    Created movie app and adde home, item and navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponentComponent } from './navigation-component/navigation-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponentComponent,
+    HomeComponentComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
