8596475bd07b54d93e54fccf7c3494b8383d18dc
nancyvgb <nancyguerrerobuelvas@gmail.com>
2020-05-01 14:20:26 -0500

    added routing
    added routing the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { TopFiveComponent } from './top-five/top-five.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponentComponent},
+  {path: 'addMovie', component: AddMovieComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'topfive', component: TopFiveComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { NavigationComponentComponent } from './navigation-component/navigation-component.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { MovieItemComponent } from './movie-item/movie-item.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { TopFiveComponent } from './top-five/top-five.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponentComponent,
     HomeComponentComponent,
-    MovieItemComponent
+    MovieItemComponent,
+    MovieDetailComponent,
+    MovieListComponent,
+    AddMovieComponent,
+    TopFiveComponent
   ],
   imports: [
     BrowserModule,
